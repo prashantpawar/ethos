320ebdf7f57323f3fde07d788cf7a6b7633fdd9d
d11e9 <d11e9@mailinator.com>
2014-08-19 22:29:37 +0000

    Uncomment osx specifi menu setup

--- static/assets/bootstrap.js
+++ static/assets/bootstrap.js
@@ -4,9 +4,9 @@ var app = gui.App;
 var win = gui.Window.get();
 var mb = new gui.Menu({type:"menubar"});
 
-// if (process.platform == 'darwin') {
-// 	mb.createMacBuiltin("Ethos");
-// 	win.menu = mb;
-// }
+if (process.platform == 'darwin') {
+	mb.createMacBuiltin("Ethos");
+	win.menu = mb;
+}
 
 win.window.location.href = 'http://eth:8080'
